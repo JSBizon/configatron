480e945b4844fbc9f8f7a124eb1f384555c0c442
Dmitry Nizovtsev <funt4k@gmail.com>
2015-01-05 16:45:10 +0200

    remove garbage

--- test-case/test-ini02.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs'),
-    ini = require('ini');
-
-var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-
-
-console.log(config);
--- test-case/test-js-yaml.js
+++
@@ -1,9 +0,0 @@
-var yaml = require('js-yaml'),
-    fs   = require('fs');
-
-try {
-    var doc = yaml.safeLoad(fs.readFileSync('./database.yml', 'utf8'));
-    console.log(doc);
-} catch (e) {
-    console.log(e);
-}
